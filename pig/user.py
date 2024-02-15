753ed3cef7090731f18833265e49bc7090664798
Pernilla Ahlström Wennerberg <pernilla.ahlstrom-wennerberg0018@stud.hkr.se>
2024-02-15 09:53:13 +0100

    New user class

---
+++ pig/user.py
@@ -0,0 +1,21 @@
+import Highscore
+
+class User:
+    def __init__(self, user_name):
+        self.user_name = user_name
+        self.highscore = Highscore
+        self.score = 0
+    
+    def change_name(self):
+        print(f"Current Username: {self.user_name}" )
+        user_name = input(f"Change {self.user_name} to: ")
+        self.user_name = user_name
+        print(f'Your usename has now been changed to {self.user_name}')
+    
+    def update_score(self, score):
+        self.score += score
+        return self.score
+    
+    def update_game_count(self, count):
+        self.game_count =+ count
+        return self.game_count
