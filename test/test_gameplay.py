ac0dadb359c5801afee75dc6e7671aa648bea684
Pernilla Ahlström Wennerberg <pernilla.ahlstrom-wennerberg0018@stud.hkr.se>
2024-02-21 21:16:02 +0100

    Appropiate tests for the gameplay.py file

--- test/test_gameplay.py
+++ test/test_gameplay.py
@@ -1,7 +1,9 @@
 import unittest
-from pig import gameplay
+from unittest.mock import *
+from pig import gameplay, user
 
 class TestGameplayClass (unittest.TestCase):
+
     def test_init_default_object(self):
         """Checks that object can be instanciated"""
         game = gameplay.Gameplay()
@@ -12,4 +14,29 @@ class TestGameplayClass (unittest.TestCase):
 
         res = game.read_from_file()
         exp = [("Pernilla", 25, 4), ("Valdemar", 28, 4)]
+        self.assertEqual(res, exp)
+
+    def test_add_two_players(self):
+        game = gameplay.Gameplay()
+
+        res = game.add_two_players()
+        exp = "Yes"
+        self.assertEqual(res, exp)
+
+    @patch('pig.user.User')
+    def test_toss(self, mock_user):
+        self.game = gameplay.Gameplay()
+        mock_user_1 = Mock()
+        mock_user_1.get_user_name.return_value = "p"
+        self.game.user_1 = mock_user_1
+
+        res = self.game.toss()
+        exp = 1
+        self.assertEqual(res, exp)
+
+
+    def test_hold(self):
+        game = gameplay.Gameplay()
+        res = game.hold()
+        exp = 2
         self.assertEqual(res, exp)
