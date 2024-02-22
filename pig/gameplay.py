02ce87336f3f3da3b915d38e3dee3cf608dba004
Pernilla Ahlström Wennerberg <pernilla.ahlstrom-wennerberg0018@stud.hkr.se>
2024-02-22 22:05:46 +0100

    Pickled the files and fixed 2 players game

--- pig/gameplay.py
+++ pig/gameplay.py
@@ -1,7 +1,7 @@
 import user
 import dice
 import highscore
-#import pickle
+import pickle
 
 
 class Gameplay:
@@ -9,31 +9,40 @@ class Gameplay:
     def __init__(self):
         self.computer_score = 0
         self.rounds = 0
-        self.file = "pig\saved_games.txt"
+        self.file = "saved_game.pickle"
         self.highscore = highscore.Highscore
         self.user_1 = None
         self.user_2 = None
         self.users_turn = 1
         self.dice = dice.Dice
+        self.round_score = 0
 
     #Saves a file if user quits before game has ended
-    def read_to_file(self):
-        with open(self.file, 'w') as file:
-            file.write(self)
+    def read_to_file(self, user):
+        atributes_to_save = {
+            "username": user.get_user_name(),
+            "highscore": user.get_highscore(),
+            "score": user.get_round_count(),
+            "game_count": user.game_count,
+            "toss_count": user.get_toss_count(),
+            "round_cont": user.get_round_count() }
+        serialized_data = pickle.dumps(atributes_to_save)
+        with open(self.file, 'wb') as file:
+            file.write(serialized_data)
         
     #Checks if a current game exists
     def read_from_file(self):
-         saved_game = []
-         with open(self.file, 'r') as file:
-            for l in file:
-                record = l.split(' ')
-                saved_game_row = (record[0], int(record[1]), int(record[2]))
-                saved_game.append(saved_game_row)
-            return saved_game
+        self.saved_game = []
+        try:
+            with open(self.file, 'rb') as file:
+                user_1, user_2 = pickle.load(file)
+                return user_1, user_2
+        except FileNotFoundError:
+            return None, None
+        except EOFError:
+            return None, None
     
-
     def add_two_players(self):
-        self.saved_game = self.read_from_file()
         self.user_name_1 = input("Enter Player 1 name: ")
         self.user_name_2 = input("Enter Player 2 name: ")
         self.user_1 = self.highscore().check_list_current_user(self.user_name_1)
@@ -49,40 +58,61 @@ class Gameplay:
         else:
             self.user_2 = user.User(self.user_name_2)
             self.user_2.game_count += 1
+        try:
+            user_1, user_2 = self.read_from_file()
+            if user_1 == self.user_name_1 or user_1 == self.user_name_2:
+                self.user_1 = user_1
+            elif user_2 == self.user_name_1 or user_2 == self.user_name_2:
+                self.user_2 = user_2
+        except FileNotFoundError:
+            pass
+
+
 
         new_game = "Yes"
-        for saved in self.saved_game:
-            if self.user_name_1 == saved[0] or self.user_name_2 == saved[0]:
-                new_game = "No"
-        return new_game
+        #Checks if there is a current game going. 
+        
+
+        return
 
     def toss(self):
-            dice_1, dice_2 = self.dice().toss()
-            dices = [dice_1, dice_2]
-            total_dice = dice_1 + dice_2
-            if self.users_turn == 1:
-                if dice_1 == 1 or dice_2 == 1:
-                    self.hold()
-                    return self.user_1, dices
-                else:
-                    print(f"{self.user_1.get_user_name()}'s turn:")
-                    self.user_1.update_score(total_dice)
-                    self.user_1.update_toss_count()
-                    return self.user_1, dices
-            elif self.users_turn == 2:
-                if dice_1 == 1 or dice_2 == 1:
-                    self.hold()
-                    return self.user_2, dices
-                else:
-                    print(f"{self.user_2.user_name}'s turn")
-                    self.user_2.update_score(total_dice)
-                    self.user_2.update_toss_count()
-                    return self.user_2, dices
-    
+        dice_1, dice_2 = self.dice().toss()
+        dices = [dice_1, dice_2]
+        if self.users_turn == 1:
+            self.user_1.update_toss_count()
+            if dice_1 == 1 and dice_2 ==1:
+                self.user_1.update_score(0)
+            if dice_1 == 1 or dice_2 == 1:
+                self.user_1.update_round_count(0)
+                self.hold()
+            else:
+                total_dices = dice_1 + dice_2
+                self.user_1.update_round_count(total_dices)
+                self.user_1.update_score(total_dices)
+            return self.user_1, dices
+        if self.users_turn == 2:
+            self.user_2.update_toss_count()
+            if dice_1 == 1 and dice_2 ==1:
+                self.user_2.update_score(0)
+            if dice_1 == 1 or dice_2 == 1:
+                self.user_2.update_round_count(0)
+                self.hold()
+            else:
+                total_dices = dice_1 + dice_2
+                self.user_2.update_round_count(total_dices)
+                self.user_2.update_score(total_dices)
+            return self.user_2, dices
+             
     def hold(self):
         if self.users_turn == 1:
             self.users_turn = 2
-            return self.users_turn
-        if self.users_turn == 2:
+            self.read_to_file(self.user_1)
+            return self.user_1
+        elif self.users_turn == 2:
             self.users_turn = 1
-        return self.users_turn
+            self.read_to_file(self.user_2)
+            return self.user_2
+    
+    def winner(self, user):
+        self.highscore.read_to_file(user)
+        return
