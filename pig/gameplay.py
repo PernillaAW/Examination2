157a5bf4c9edc9c4be809ca68ca59a3b138e3440
Pernilla Ahlström Wennerberg <pernilla.ahlstrom-wennerberg0018@stud.hkr.se>
2024-02-24 21:47:12 +0100

    Divided the class to smaller modules

--- pig/gameplay.py
+++ pig/gameplay.py
@@ -13,7 +13,7 @@ class Gameplay:
         self.user_1 = None
         self.user_2 = None
         self.users_turn = 1
-        self.dice = dice.Dice
+        self.dice = dice.Dice()
         self.round_score = 0
 
     #Saves a file if user quits before game has ended
@@ -44,6 +44,13 @@ class Gameplay:
         """Adds two players to the game, if they already exists their score is loaded"""
         user_name_1 = input("Enter Player 1 name: ")
         user_name_2 = input("Enter Player 2 name: ")
+        users = [user_name_1, user_name_2]
+        return users
+    
+    def check_if_user_exists(self, users):
+        """Check if the user exists"""
+        user_name_1 = users[0]
+        user_name_2 = users[1]
         self.user_1 = self.highscore().check_list_current_user(user_name_1)
         if self.user_1 is not None:
             self.user_1.game_count += 1
@@ -57,6 +64,12 @@ class Gameplay:
         else:
             self.user_2 = user.User(user_name_2)
             self.user_2.game_count += 1
+        return users
+
+    def check_saved_game(self, users):
+        """Check to see if there is a game saved"""
+        user_name_1 = users[0].get_user_name()
+        user_name_2 = users[1].get_user_name()
         try:
             user_to_load = self.read_from_file()
             if user_to_load[0] and user_to_load[1] is not None:
@@ -66,14 +79,25 @@ class Gameplay:
                 if user_to_load[1].get_user_name() == user_name_1 or \
                      user_to_load[1].get_user_name() == user_name_2:
                     self.user_2 = user_to_load[1]
+            else:
+                self.user_1 = user.User(users[0])
+                self.user_1 = user.User(users[1])                    
+            users[0] = self.user_1
+            users[1] = self.user_2
         except FileNotFoundError:
             pass
-        return self.user_1, self.user_2
+        return users
 
     def toss(self):
         """Where dices gets tossed, added up and saved s"""
-        dice_1, dice_2 = self.dice().toss()
-        dices = [dice_1, dice_2]
+        dice_1, dice_2 = self.dice.toss()
+        dices = (dice_1, dice_2)
+        return dices
+    
+    def update_user_score(self, dices):
+        """Updates the score for user, also handles if user toss one or two one's"""
+        dice_1 = dices[0]
+        dice_2 = dices[1]
         if self.users_turn == 1:
             self.user_1.update_toss_count()
             if dice_1 == 1 and dice_2 ==1:
@@ -85,7 +109,7 @@ class Gameplay:
                 total_dices = dice_1 + dice_2
                 self.user_1.update_round_count(total_dices)
                 self.user_1.update_score(total_dices)
-            return self.user_1, dices
+            return self.user_1
         if self.users_turn == 2:
             self.user_2.update_toss_count()
             if dice_1 == 1 and dice_2 ==1:
@@ -97,9 +121,10 @@ class Gameplay:
                 total_dices = dice_1 + dice_2
                 self.user_2.update_round_count(total_dices)
                 self.user_2.update_score(total_dices)
-            return self.user_2, dices
-             
+            return self.user_2
+
     def hold(self):
+        """ Saves the users and changes turn """
         if self.users_turn == 1:
             self.users_turn = 2
             self.read_to_file(self.user_1, self.user_2)
@@ -108,8 +133,9 @@ class Gameplay:
             self.users_turn = 1
             self.read_to_file(self.user_1, self.user_2)
             return self.user_1
-    
+
     def winner(self, user_to_save):
+        """When there is a winner their highscor shall be saved"""
         self.read_to_file(None, None)
         self.highscore.read_to_file(user_to_save)
-        return
+    
