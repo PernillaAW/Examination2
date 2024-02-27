70fc96003d296a46c0626427a58867d8d3d9a7a5
Pernilla Ahlström Wennerberg <pernilla.ahlstrom-wennerberg0018@stud.hkr.se>
2024-02-27 13:09:09 +0100

    changes

--- pig/shell.py
+++ pig/shell.py
@@ -14,7 +14,7 @@ class Shell(cmd.Cmd):
         self.game = gameplay.Gameplay()
         self.two_player = ''
         self.intelligence = Intellegance.Intellegance()
-        self.user_1 = user.User
+        self.user_comp_1 = user.User
         self.computer = user.User
 
     def do_players(self, arg):
@@ -30,7 +30,7 @@ class Shell(cmd.Cmd):
             level = input("At what level do you want to start?")
             self.intelligence.level_choice(level)
             user_name = input("What is your name?: ")
-            self.user_1 = user.User(user_name)
+            self.user_comp_1 = user.User(user_name)
             self.computer = user.User('Computer')
             self.two_player = "No"
         elif arg == '2':
@@ -68,8 +68,29 @@ class Shell(cmd.Cmd):
                 else:
                     print(f'\n{user.get_user_name()} has {user.get_round_count()}'
                         f'points this round, Toss or Hold?.\n')
+        #Plays against computer
         elif self.two_player == "No":
-            computer_score = self.intelligence.toss_or_hold(self.user_1)
+            self.user_comp_1 = self.game.update_user_score(dices)
+            print(f'Dices rolled: {dices[0]}, {dices[1]}')
+            if dices[0] == 1 or dices[1] == 1:
+                print(f"\nBad luck, you rolled a 1. {self.user_comp_1.get_user_name()}"
+                      f"no points this round. \n")
+            elif dices[0] == 1 and dices[1] == 1:
+                print(f"\nNo, two ones, all {user.get_user_name()} points disapear. \n")
+            else:
+                if user.get_score() >= 100:
+                    print(f"{user.get_user_name()} has won the game! Congratulations.")
+                    self.game.winner(user)
+                    return
+                else:
+                    print(f'\n{user.get_user_name()} has {user.get_round_count()}'
+                        f'points this round, Toss or Hold?.\n')
+
+
+
+            computer_score = self.intelligence.toss_or_hold(self.user_comp_1)
+            self.computer.update_score(computer_score)
+            
             
         else:
             print(error_msg)
