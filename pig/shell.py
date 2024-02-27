102db2953279121c834c519081681919c7eb747d
Pernilla Ahlström Wennerberg <pernilla.ahlstrom-wennerberg0018@stud.hkr.se>
2024-02-27 11:27:55 +0100

    connected with computer intelligence

--- pig/shell.py
+++ pig/shell.py
@@ -1,6 +1,6 @@
+"""Shell this is what the user see"""
 import cmd
-import dice
-import gameplay
+import dice, gameplay, Intellegance, user
 
 class Shell(cmd.Cmd):
     """The introduction to a command driven game of dice"""
@@ -13,14 +13,16 @@ class Shell(cmd.Cmd):
         self.dice = dice.Dice()
         self.game = gameplay.Gameplay()
         self.new_game = "No"
+        self.intelligence = Intellegance.Intellegance()
+        self.user = user.User
 
     #def do_start(self, _):
         """Start a game of pig."""
         #self.game.
 
         # Calls a class.method that starts the game
-        
-    
+
+
     def do_players(self, arg):
         """
         User decides to play against computer or another player 
@@ -28,44 +30,55 @@ class Shell(cmd.Cmd):
         'players 2' - Play against a friend 
         """
         error_message ="Invalid choice. Write 'players 1' or 'players 2'"
-        if (arg == '1'):
+        if arg == '1':
             # Calls the computer intelligence
             print('You have choosen to play against the computer')
-        elif (arg == '2'):
+            level = input("At what level do you want to start?")
+            self.intelligence.level_choice(level)
+            user_name = input("What is your name?: ")
+            user_1 = user.User(user_name)
+            computer = user.User('Computer')
+            self.intelligence.toss_or_hold(user_1)
+            print(self.intelligence.score)
+
+        elif arg == '2':
             print('You have choosen to play against another player.')
-            user_1, user_2 = self.game.add_two_players()
-            print(f"Welcome {user_1.get_user_name()} and \
-                  {user_2.get_user_name()}! {user_1.get_user_name()} starts. Write toss to \
-                    start the game.")
+            users = self.game.add_two_players()
+            users = self.game.check_if_user_exists(users)
+            users = self.game.check_saved_game(users)
+            user_1 = users[0]
+            user_2 = users[1]
+            print(f"Welcome {user_1.user_name()} and "
+                  f"{user_2.user_name()}! \n {user_1.user_name()} starts. Write toss to"
+                    f"start the game.")
             self.new_game = "Yes"
-        else: 
-            print(error_message)
-        return
-    
-    def do_toss(self, _):
-        """Toss the dices for you"""
-        error_message ="Please choose 'players 1' or 'players 2' first."
-        if self.new_game == "Yes":
-            user, dices = self.game.toss()
-            print(f'Dices rolled: {dices[0]}, {dices[1]}')
-            if dices[0] == 1 or dices[1] == 1:
-                print(f"\nBad luck, you rolled a 1. {user.get_user_name()} no points this round. \n")
-            elif dices[0] == 1 and dices[1] == 1:
-                print(f"\nNo, two ones, all {user.get_user_name()} points disapear. \n")
-            else:
-                if user.get_score() >= 100:
-                    print(f"{user.get_user_name()} has won the game! Congratulations.")
-                    self.game.winner(user)
-                    return
-                else:
-                    print(f'\n{user.get_user_name()} has {user.get_round_count()} points this round, Toss or Hold?.\n')
         else:
             print(error_message)
-    
+        return
+
+    def do_toss(self, _):
+        """Toss the dices for you"""
+        dices = self.game.toss()
+        user = self.game.update_user_score(dices)
+        print(f'Dices rolled: {dices[0]}, {dices[1]}')
+        if dices[0] == 1 or dices[1] == 1:
+            print(f"\nBad luck, you rolled a 1. {user.get_user_name()}"
+                  f"no points this round. \n")
+        elif dices[0] == 1 and dices[1] == 1:
+            print(f"\nNo, two ones, all {user.get_user_name()} points disapear. \n")
+        else:
+            if user.get_score() >= 100:
+                print(f"{user.get_user_name()} has won the game! Congratulations.")
+                self.game.winner(user)
+                return
+            else:
+                print(f'\n{user.get_user_name()} has {user.get_round_count()}'
+                    f'points this round, Toss or Hold?.\n')
+
     def do_hold(self, _):
         """Holds the game and start tallys the score and saves it """
         user = self.game.hold()
-        print(f"\n{user.get_user_name()}'s turn to play. Your total score is " 
+        print(f"\n{user.get_user_name()}'s turn to play. Your total score is "
               f"{user.get_score()} and you have tossed {user.get_toss_count()} times.\n")
 
 
