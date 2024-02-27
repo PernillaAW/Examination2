784f98839c3b651d3110964bc75e1108fd7451d5
Nikita Fransson Ünver <160026549+unvernikita@users.noreply.github.com>
2024-02-27 12:58:31 +0100

    Correcting merging errors

---
+++ pig/gameplay.py
@@ -0,0 +1,22 @@
+import user
+import highscore
+
+
+class Gameplay:
+
+    def __init__(self):
+        self.computer_score = 0
+        self.rounds = 0
+        self.file = "saved_games.txt"
+        self.player = user.User
+
+    #  Saves a file if user quits before game has ended
+    def read_to_file(self):
+        with open(self.file, 'w') as file:
+            file.write(self)
+
+    #  Checks if a current game exists
+    def read_from_file(self):
+        with open(self.file, 'r') as file:
+            for list in file:
+                current_game = list
--- pig/shell.py
+++ pig/shell.py
@@ -1,7 +1,12 @@
 import cmd
+<<<<<<< HEAD
 import Dice
 import Intellegance
 import Highscore
+=======
+import user
+import dice
+>>>>>>> parent of 14000a2 (Solved merge conflicts)
 
 
 class Shell(cmd.Cmd):
@@ -12,6 +17,7 @@ class Shell(cmd.Cmd):
     def __init__(self):
         """Instansiate the object"""
         super().__init__()
+<<<<<<< HEAD
         self.dice = Dice.Dice()
 
     def do_start(self, _):
@@ -31,6 +37,18 @@ class Shell(cmd.Cmd):
         player.change_name(player, new_name)
         Highscore.Highscore().check_list_add_remove(player, old_username)
         print(f'Your usename has now been changed to {player.get_user_name}')
+=======
+        self.dice = dice.Dice()
+
+    def do_start(self, _):
+        """Start a game of pig."""
+        player_1 = input("What is you name? ")
+        # check if player_1 exist
+        dice_1, dice_2 = self.dice.toss()
+        print(f"{dice_1}, {dice_2}")
+
+        # Calls a class.method that starts the game
+>>>>>>> parent of 14000a2 (Solved merge conflicts)
 
     def do_players(self, arg):
         """
@@ -40,6 +58,7 @@ class Shell(cmd.Cmd):
         """
         error_message = "Invalid choice. Write 'players 1' or 'players 2'"
         if (arg == '1'):
+<<<<<<< HEAD
             computer = Intellegance.Intellegance
             print('You have choosen to play against the computer')
             choise = input('Now chose game level low, medium, hard: ')
@@ -50,16 +69,27 @@ class Shell(cmd.Cmd):
             playerlist.read_from_file()
             player_2 = input("What is you name? ")
             player_2 = playerlist.check_list_current_user(player_2)
+=======
+            # Calls the computer intelligence
+            print('You have choosen to play against the computer')
+        elif (arg == '2'):
+            print('You have choosen to play against another player.')
+            # Calls add second player
+>>>>>>> parent of 14000a2 (Solved merge conflicts)
         else:
             print(error_message)
 
     def do_quit(self, _):
         """Quits the game"""
+<<<<<<< HEAD
         Highscore.Highscore().read_to_file()
+=======
+>>>>>>> parent of 14000a2 (Solved merge conflicts)
         return True
 
     def do_exit(self, _):
         """Exits the game"""
+<<<<<<< HEAD
         Highscore.Highscore().read_to_file()
         return True
 
@@ -73,3 +103,10 @@ class Shell(cmd.Cmd):
         Highscore.Highscore().read_from_file() \
             .sort_player_highscore() \
                  .display()
+=======
+        return True
+
+    def do_cheat(self, _):
+        """A cheat to directly win the game (for testing purposes only)"""
+        print("Cheater.. cheater..")
+>>>>>>> parent of 14000a2 (Solved merge conflicts)
